117e825248d03f3cf5932afe14cebc82f9677902
Shakeeb Shaikh <eren.85yeager@gmail.com>
2025-11-11 20:31:10 +0530

    Create personals.py

---
+++ config/personals.py
@@ -0,0 +1,20 @@
+'''
+Author:     Shakeeb Shaikh
+LinkedIn:  https://www.linkedin.com/in/shakeeb-shaikh-02b32b282/=
+            
+GitHub:     https://github.com/ShakeebSk
+'''
+
+first_name = "Your first name like Alex"                
+middle_name = "Your Middle name"         
+last_name = "your last name"
+phone_number = "your phone number"      
+current_city = "your current city where u live in"              
+street = "your current address"
+state = "enter the state where u live in"
+zipcode = "locality code"
+country = "your country"
+ethnicity = "Decline"   
+gender = "your gender"  
+disability_status = "Decline"      
+veteran_status = "Decline"      
